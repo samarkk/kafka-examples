65fc29033ca693c462245da88ee16f4445e0f5c1
The Learner <samarkant1@yahoo.com>
2020-11-02 17:23:12 +0530

    Update ProducerCustomer.java

--- src/main/java/customserde/ProducerCustomer.java
+++ src/main/java/customserde/ProducerCustomer.java
@@ -1,3 +1,5 @@
+package customserde;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
