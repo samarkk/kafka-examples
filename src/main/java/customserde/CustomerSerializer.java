f9835388747e839ce0d0698d98f4a206623529fc
The Learner <samarkant1@yahoo.com>
2020-11-02 17:22:27 +0530

    Update CustomerSerializer.java

--- src/main/java/customserde/CustomerSerializer.java
+++ src/main/java/customserde/CustomerSerializer.java
@@ -1,3 +1,5 @@
+package customserde;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.common.serialization.Serializer;
