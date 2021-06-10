89d0ebf982ebddefd9e8cee23521ccf81b1e236b
The Learner <samarkant1@yahoo.com>
2021-06-10 16:32:42 +0530

    Update ConsumerDemo.java

--- src/main/java/consumer/ConsumerDemo.java
+++ src/main/java/consumer/ConsumerDemo.java
@@ -1,49 +1,69 @@
 package consumer;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.AlterConsumerGroupOffsetsOptions;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class ConsumerDemo {
-    public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
+    // create the consumer configurations
+    static String bservers = "192.168.181.138:9092";
+    static String groupId = "idegr";
+    static String topic = "ide_topic";
 
-        // build a configutation
+    private static Properties createConsumerConfiguration() {
         Properties props = new Properties();
-        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "ideappg");
-
-        // create a consumer
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
-
-        consumer.subscribe(Collections.singletonList("ide_topic"));
-        // consumer will poll from the topic
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-            for (ConsumerRecord<String, String> record : records) {
-                String valToPrint = "Key: " + record.key() +
-                        "\nValue: " + record.value() +
-                        "\nPartition: " + record.partition() +
-                        "\nOffset: " + record.offset();
-//                logger.info("Key: " + record.key() +
-//                "\nValue: " + record.value() +
-//                        "\nPartition: " + record.partition() +
-//                        "\nOffset: " + record.offset());
-                System.out.println(valToPrint);
-
-            }
-        }
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bservers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class.getName());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        // Group id has to be provided
+        //Exception in thread "main" org.apache.kafka.common.errors.InvalidGroupIdException: To use the group management or offset commit APIs, you must provide a valid group.id in the consumer configuration
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        return props;
     }
 
+    // create a consumer from configurations
+    private static KafkaConsumer<String, String> createKafkaConsumer() {
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String,
+                String>(createConsumerConfiguration());
+        return consumer;
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        // subscribe to topic, topics
+        KafkaConsumer<String, String> consumer = createKafkaConsumer();
+        consumer.subscribe(Collections.singleton(topic));
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                    if (records.count() > 0) {
+                        for (ConsumerRecord<String, String> record : records) {
+                            String recordVal = record.value().toUpperCase();
+                            System.out.println("Record partition: " + record.partition() +
+                                    ", offset: " + record.offset() + ", value " +
+                                    "processed: " + recordVal);
+                        }
+                        consumer.commitSync();
+                    }
+            }
+        } catch (Exception ex) {
+            System.out.println("Some exception happened");
+            ex.printStackTrace();
+        } finally {
+            consumer.close();
+        }
+    }
 }
