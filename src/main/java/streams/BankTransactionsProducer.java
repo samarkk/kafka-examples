870d7b2beae481445e1c5a5b9e120c023305c12b
The Learner <samarkant1@yahoo.com>
2021-12-10 10:57:22 +0530

    Add files via upload

--- src/main/java/streams/BankTransactionsProducer.java
+++ src/main/java/streams/BankTransactionsProducer.java
@@ -17,29 +17,38 @@ public class BankTransactionsProducer {
         Properties properties = new Properties();
 
         // kafka bootstrap server
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "master.e4rlearning.com:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         // producer acks
         properties.setProperty(ProducerConfig.ACKS_CONFIG, "all"); // strongest producing guarantee
+//        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION,"1");
         properties.setProperty(ProducerConfig.RETRIES_CONFIG, "3");
         properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "1");
         // leverage idempotent producer from Kafka 0.11 !
-        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true"); // ensure we don't push duplicates
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "testtrans");
+        // ensure we don't push duplicates
 
-        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
-
-        int i = 0;
+        Producer<String, String> producer = new KafkaProducer<>(properties);
+        producer.initTransactions();
+        int i = 1;
         while (true) {
             System.out.println("Producing batch: " + i);
             try {
-                producer.send(newRandomTransaction("ram"));
-                Thread.sleep(100);
-                producer.send(newRandomTransaction("shyam"));
-                Thread.sleep(100);
-                producer.send(newRandomTransaction("sohan"));
-                Thread.sleep(100);
-                i += 1;
+                producer.beginTransaction();
+                int pause = Integer.parseInt(args[0]);
+                producer.send(newRandomTransaction("ram", i++));
+                Thread.sleep(pause);
+                producer.send(newRandomTransaction("shyam", i++));
+                Thread.sleep(pause);
+                producer.send(newRandomTransaction("sohan", i++));
+                Thread.sleep(pause);
+                producer.send(newRandomTransaction("radha", i++));
+                Thread.sleep(pause);
+                producer.send(newRandomTransaction("mona", i++));
+                Thread.sleep(pause);
+                producer.commitTransaction();
             } catch (InterruptedException e) {
                 break;
             }
@@ -47,7 +56,7 @@ public class BankTransactionsProducer {
         producer.close();
     }
 
-    public static ProducerRecord<String, String> newRandomTransaction(String name) {
+    public static ProducerRecord<String, String> newRandomTransaction(String name, int amt) {
         // creates an empty json {}
         ObjectNode transaction = JsonNodeFactory.instance.objectNode();
 
@@ -59,7 +68,7 @@ public class BankTransactionsProducer {
 
         // we write the data to the json document
         transaction.put("name", name);
-        transaction.put("amount", amount);
+        transaction.put("amount", amt);
         transaction.put("time", now.toString());
         return new ProducerRecord<>("bank-transactions", name, transaction.toString());
     }
